cca9c3a7abf1dff6ea1887ae70bce66407f20a3a
Songvut Nakrong <songvut.nakrong@gmail.com>
2022-06-29 08:01:30 +0700

    Remove unused data except projects

--- src/data.js
+++ src/data.js
@@ -1,187 +1,70 @@
-export const information = {
-  image: "/assets/profile.webp",
-  name: "songvut nakrong",
-  position: "software engineer",
-  summary: "Hi! I'm 24 years old, A fresh graduate seeking a full-time position in the field of web development, Where I can apply my knowledge and skills for continuous improvement.",
-  email: "songvut.nakrong@gmail.com",
-  phone: "096-615-3710",
-  address: "Bangkok, Thailand",
-  githubLink: "https://www.github.com/songvut365",
-  linkedInLink: "https://www.linkedin.com/in/songvut-nakrong/",
-  facebookLink: "https://www.facebook.com/songvut365/",
-  instagramLink: "https://www.instagram.com/songvut.365/",
-}
-
-export const skills = [
-  {
-    "name": "Programming Language",
-    "details": [
-      {
-        "name": "Go",
-        "image": "/assets/icon/go.webp"
-      },
-      {
-        "name": "Javascript",
-        "image": "/assets/icon/javascript.webp"
-      },
-      {
-        "name": "Python",
-        "image": "/assets/icon/python.webp"
-      },
-    ]
-  },
-  {
-    "name": "Back-end",
-    "details": [
-      {
-        "name": "Fiber",
-        "image": "/assets/icon/fiber.webp"
-      },
-      {
-        "name": "Express.js",
-        "image": "/assets/icon/express.webp"
-      },
-      {
-        "name": "Django",
-        "image": "/assets/icon/django.webp"
-      },
-    ]
-  },
-  {
-    "name": "Front-end",
-    "details": [
-      {
-        "name": "React.js",
-        "image": "/assets/icon/react.webp"
-      },
-      {
-        "name": "Material UI",
-        "image": "/assets/icon/mui.webp"
-      },
-      {
-        "name": "Vue.js",
-        "image": "/assets/icon/vue.webp"
-      },
-      {
-        "name": "Vuetify",
-        "image": "/assets/icon/vuetify.webp"
-      },
-      {
-        "name": "Tailwind CSS",
-        "image": "/assets/icon/tailwind.webp"
-      },
-    ]
-  },
-  {
-    "name": "Database",
-    "details": [
-      {
-        "name": "MySQL",
-        "image": "/assets/icon/mysql.webp"
-      },
-      {
-        "name": "MongoDB",
-        "image": "/assets/icon/mongodb.webp"
-      },
-      {
-        "name": "SQLite",
-        "image": "/assets/icon/sqlite.webp"
-      },
-    ]
-  },
-  {
-    "name": "Tools",
-    "details": [
-      {
-        "name": "Docker",
-        "image": "/assets/icon/docker.webp"
-      },
-      {
-        "name": "RabbitMQ",
-        "image": "/assets/icon/rabbitmq.webp"
-      },
-      {
-        "name": "Postman",
-        "image": "/assets/icon/postman.webp"
-      },
-      {
-        "name": "Figma",
-        "image": "/assets/icon/figma.webp"
-      },
-    ]
-  },
-  {
-    "name": "Language Proficiency",
-    "details": [
-      {
-        "name": "Thai",
-        "short": "TH",
-        "level": "Native",
-        "color": "bg-red-800"
-      },
-      {
-        "name": "English",
-        "short": "EN",
-        "level": "CEFR B1",
-        "color": "bg-blue-900"
-      },
-    ]
-  },
-]
-
-export const education = [
-  {
-    "name": "King Mongkut's Institute of Technology Ladkrabang",
-    "degree": "Bachelor of Engineering",
-    "major": "Computer Engineering",
-    "date": "Aug 2019 - May 2022",
-    "gpa": "3.50",
-  },
-  {
-    "name": "Hatyai Technical College",
-    "degree": "High Vocational Certificate",
-    "major": "Computer Technology",
-    "date": "May 2016 - Mar 2018",
-    "gpa": "3.59",
-  }
-]
-
 export const projects = [
   {
-    "name": "VCAPTCHA",
-    "image": "/assets/project/vcaptcha.webp",
-    "detail": "Human Verification Service using Voice Recognition The purpose is to create a system to determine that users are human instead of using images",
-    "link": "https://vcaptcha.work/",
+    id: 1,
+    name: "VCAPTCHA",
+    cover: "/assets/project/vcaptcha.webp",
+    image: "/assets/project/vcaptcha.webp",
+    detail:
+      "Human Verification Service using Voice Recognition The purpose is to create a system to determine that users are human instead of using images",
+    link: "https://vcaptcha.work/",
+  },
+  // {
+  //   id: 2,
+  //   name: "Songvut Website",
+  //   cover: "",
+  //   image: "",
+  //   detail: "",
+  //   link: "",
+  // },
+  {
+    id: 3,
+    name: "Nihon Vocabulary Server",
+    cover: "/assets/project/nihon-vocab.webp",
+    image: "/assets/project/nihon-vocab.webp",
+    detail: "Server for Nihon Vocabulary, Japanese vocabulary application API",
+    link: "https://github.com/songvut365/nihon-vocabulary-server",
   },
   {
-    "name": "Nihon Vocabulary Server",
-    "image": "/assets/project/nihon-vocab.webp",
-    "detail": "Server for Nihon Vocabulary, Japanese vocabulary application API",
-    "link": "https://github.com/songvut365/nihon-vocabulary-server",
+    id: 4,
+    name: "Election Application",
+    cover: "/assets/project/election-app.webp",
+    image: "/assets/project/election-app.webp",
+    detail: "Voting application with React and Golang, Microservice design",
+    link: "https://github.com/songvut365/election-application",
   },
   {
-    "name": "Election Application",
-    "image": "/assets/project/election-app.webp",
-    "detail": "Voting application with React and Golang, Microservice design",
-    "link": "https://github.com/songvut365/election-application",
+    id: 5,
+    name: "Search Engine",
+    cover: "/assets/project/search-engine.webp",
+    image: "/assets/project/search-engine.webp",
+    detail:
+      "Search Engine Web Application with Django and Elasticsearch, Searching Information about COVID-19",
+    link: "https://github.com/songvut365/search-engine-django-elasticsearch",
   },
   {
-    "name": "Search Engine",
-    "image": "/assets/project/search-engine.webp",
-    "detail": "Search Engine Web Application with Django and Elasticsearch, Searching Information about COVID-19",
-    "link": "https://github.com/songvut365/search-engine-django-elasticsearch",
+    id: 6,
+    name: "Game Store",
+    cover: "/assets/project/game-store.webp",
+    image: "/assets/project/game-store.webp",
+    detail:
+      "Game Store with ASP .NET MVC, Online Store, Shopping cart and Payment",
+    link: "https://github.com/songvut365/GameStore",
   },
   {
-    "name": "Game Store",
-    "image": "/assets/project/game-store.webp",
-    "detail": "Game Store with ASP .NET MVC, Online Store, Shopping cart and Payment",
-    "link": "https://github.com/songvut365/GameStore",
+    id: 7,
+    name: "DFA ATM",
+    cover: "/assets/project/dfa-atm.webp",
+    image: "/assets/project/dfa-atm.webp",
+    detail:
+      "Automatic Teller Machine using Deterministic Finite Automaton, Web application",
+    link: "https://github.com/songvut365/DFA-ATM",
   },
-  {
-    "name": "DFA ATM",
-    "image": "/assets/project/dfa-atm.webp",
-    "detail": "Automatic Teller Machine using Deterministic Finite Automaton, Web application",
-    "link": "https://github.com/songvut365/DFA-ATM",
-  },
-  
-]
+  // {
+  //   id: 8,
+  //   name: "Golang CRUD",
+  //   cover: "",
+  //   image: "",
+  //   detail: "",
+  //   link: "",
+  // },
+];
