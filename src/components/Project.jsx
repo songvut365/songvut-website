61dfd19573ab37a1bad3bbfd322570d800766805
Songvut Nakrong <songvut.nakrong@gmail.com>
2022-06-30 19:20:09 +0700

    Get projects from firebase and Add skeleton for waiting to loading

--- src/components/Project.jsx
+++ src/components/Project.jsx
@@ -1,21 +1,46 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import Fade from 'react-reveal/Fade';
-import { projects } from '../data';
+
+import { db } from '../main';
+import { collection, getDocs, query, orderBy } from "firebase/firestore";
 
 import ProjectCard from './ProjectCard';
 
 export default function Project() {
+  const [projects, setProjects] = useState([]);
+
+  const getProjects = async () => {
+    let allProjects = [];
+    const q = query(collection(db, "projects"), orderBy("id"))
+    const querySnapshot = await getDocs(q);
+    querySnapshot.forEach((doc) => {
+      allProjects.push(doc.data())
+    })
+
+    setProjects(allProjects);
+  }
+
+  useEffect(() => {
+    getProjects();
+  }, []);
+
   return (
     <div className='py-4'>
       <Fade>
 
       <h1 className='text-3xl font-semibold text-yellow-300 mb-4'>PROJECTS</h1>
       
-      <div className='md:grid md:justify-center lg:grid-cols-2  gap-4'>
-        {projects.map(project => (
-          <ProjectCard key={project.name} project={project} />
-        ))}
-      </div>
+      {projects.length > 0 
+      ? <div className='md:grid md:justify-center lg:grid-cols-2  gap-4'>
+          {projects.map(project => (
+            <ProjectCard key={project.name} project={project} />
+          ))}
+        </div>
+      : <div className='md:grid md:justify-center lg:grid-cols-2  gap-4'>
+          <ProjectCard />
+          <ProjectCard />
+        </div>
+      }
       
       <div className='text-center mt-8'>
         <a href="https://github.com/songvut365" target="_blank"
